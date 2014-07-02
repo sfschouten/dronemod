c5e1783e552fab7b8e28ed3e9598da230b24a152
Stefan Schouten <stefanschouten10@gmail.com>
2014-07-03 00:04:10 +0200

    Changed rendering back to the non-.obj version

--- src/main/java/sfschouten/dronemod/client/renderer/entity/RenderHexacopter.java
+++ src/main/java/sfschouten/dronemod/client/renderer/entity/RenderHexacopter.java
@@ -1,58 +1,47 @@
 package sfschouten.dronemod.client.renderer.entity;
 
-import sfschouten.dronemod.DroneMod;
-import sfschouten.dronemod.Logger;
-import sfschouten.dronemod.entity.EntityAluminiumMediumHexacopter;
-import sfschouten.dronemod.entity.EntityAluminiumMediumQuadcopter;
-import sfschouten.dronemod.entity.EntityAluminiumStrongHexacopter;
-import sfschouten.dronemod.entity.EntityAluminiumStrongQuadcopter;
-import sfschouten.dronemod.entity.EntityCaneWeakHexacopter;
-import sfschouten.dronemod.entity.EntityCaneWeakQuadcopter;
-import sfschouten.dronemod.entity.EntityDrone;
-import sfschouten.dronemod.entity.EntityWoodMediumHexacopter;
-import sfschouten.dronemod.entity.EntityWoodMediumQuadcopter;
-import sfschouten.dronemod.entity.EntityWoodStrongHexacopter;
-import sfschouten.dronemod.entity.EntityWoodStrongQuadcopter;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.AdvancedModelLoader;
 import net.minecraftforge.client.model.IModelCustom;
+import sfschouten.dronemod.DroneMod;
+import sfschouten.dronemod.Logger;
+import sfschouten.dronemod.entity.EntityAluminiumMediumHexacopter;
+import sfschouten.dronemod.entity.EntityAluminiumStrongHexacopter;
+import sfschouten.dronemod.entity.EntityCaneWeakHexacopter;
+import sfschouten.dronemod.entity.EntityDrone;
+import sfschouten.dronemod.entity.EntityWoodMediumHexacopter;
+import sfschouten.dronemod.entity.EntityWoodStrongHexacopter;
 
-public class RenderHexacopter extends RenderDrone {
+public class RenderHexacopter extends RenderLiving {
 
-	private static final ResourceLocation woodHexaTextureLocation = new ResourceLocation(DroneMod.modID + ":textures/entity/woodHexa.png");
-	private static final ResourceLocation caneHexaTextureLocation = new ResourceLocation(DroneMod.modID + ":textures/entity/caneHexa.png");
-	private static final ResourceLocation aluminiumHexaTextureLocation = new ResourceLocation(DroneMod.modID + ":textures/entity/aluminiumHexa.png");
-	
-	private static final ResourceLocation hexaModelLocation = new ResourceLocation(DroneMod.modID + ":models/entity/hexa.obj");
-	private static final IModelCustom hexaModel = AdvancedModelLoader.loadModel(hexaModelLocation);
-	
-	public RenderHexacopter() {
-		super(hexaModel);
-	}
+	private static final ResourceLocation woodHexaTextureLocation = new ResourceLocation(DroneMod.modID, "textures/entity/woodHexa.png");
+	private static final ResourceLocation caneHexaTextureLocation = new ResourceLocation(DroneMod.modID, "textures/entity/caneHexa.png");
+	private static final ResourceLocation aluminiumHexaTextureLocation = new ResourceLocation(DroneMod.modID, "textures/entity/aluminiumHexa.png");
 
-	@Override
-	public void doRender(Entity var1, double x, double y, double z, float var8, float var9) {
-		// Render chests, modules, batteries in appropriate locations for hexacopters.
-		super.doRender(var1, x, y, z, var8, var9);
+	//TODO change ModelBase to HexaModel
+	public RenderHexacopter(ModelBase par1ModelBase, float par2) {
+		super(par1ModelBase, par2);
 	}
 
 	@Override
 	protected ResourceLocation getEntityTexture(Entity var1) {
 		EntityDrone d = null;
-		try{
+		try {
 			d = (EntityDrone) var1;
-		}catch(ClassCastException e){
+		} catch (ClassCastException e) {
 			e.printStackTrace();
 		}
-		
-		if(d instanceof EntityAluminiumMediumHexacopter || d instanceof EntityAluminiumStrongHexacopter) {
+
+		if (d instanceof EntityAluminiumMediumHexacopter || d instanceof EntityAluminiumStrongHexacopter) {
 			return aluminiumHexaTextureLocation;
-		}else if(d instanceof EntityWoodMediumHexacopter || d instanceof EntityWoodStrongHexacopter ){
+		} else if (d instanceof EntityWoodMediumHexacopter || d instanceof EntityWoodStrongHexacopter) {
 			return woodHexaTextureLocation;
-		}else if(d instanceof EntityCaneWeakHexacopter){
+		} else if (d instanceof EntityCaneWeakHexacopter) {
 			return caneHexaTextureLocation;
-		}else{
+		} else {
 			Logger.logOut("Wrong type of drone, not a Hexa! No texture loaded.");
 			return null;
 		}
--- src/main/java/sfschouten/dronemod/client/renderer/entity/RenderOctacopter.java
+++ src/main/java/sfschouten/dronemod/client/renderer/entity/RenderOctacopter.java
@@ -1,43 +1,27 @@
 package sfschouten.dronemod.client.renderer.entity;
 
-import sfschouten.dronemod.DroneMod;
-import sfschouten.dronemod.Logger;
-import sfschouten.dronemod.entity.EntityAluminiumMediumHexacopter;
-import sfschouten.dronemod.entity.EntityAluminiumMediumOctacopter;
-import sfschouten.dronemod.entity.EntityAluminiumMediumQuadcopter;
-import sfschouten.dronemod.entity.EntityAluminiumStrongHexacopter;
-import sfschouten.dronemod.entity.EntityAluminiumStrongOctacopter;
-import sfschouten.dronemod.entity.EntityAluminiumStrongQuadcopter;
-import sfschouten.dronemod.entity.EntityCaneWeakHexacopter;
-import sfschouten.dronemod.entity.EntityCaneWeakQuadcopter;
-import sfschouten.dronemod.entity.EntityDrone;
-import sfschouten.dronemod.entity.EntityWoodMediumHexacopter;
-import sfschouten.dronemod.entity.EntityWoodMediumOctacopter;
-import sfschouten.dronemod.entity.EntityWoodMediumQuadcopter;
-import sfschouten.dronemod.entity.EntityWoodStrongHexacopter;
-import sfschouten.dronemod.entity.EntityWoodStrongOctacopter;
-import sfschouten.dronemod.entity.EntityWoodStrongQuadcopter;
+import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.AdvancedModelLoader;
-import net.minecraftforge.client.model.IModelCustom;
+import sfschouten.dronemod.DroneMod;
+import sfschouten.dronemod.Logger;
+import sfschouten.dronemod.client.model.ModelOctaCopter;
+import sfschouten.dronemod.entity.EntityAluminiumMediumOctacopter;
+import sfschouten.dronemod.entity.EntityAluminiumStrongOctacopter;
+import sfschouten.dronemod.entity.EntityDrone;
+import sfschouten.dronemod.entity.EntityWoodMediumOctacopter;
+import sfschouten.dronemod.entity.EntityWoodStrongOctacopter;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
-public class RenderOctacopter extends RenderDrone {
-
-	private static final ResourceLocation woodOctaTextureLocation = new ResourceLocation(DroneMod.modID + ":textures/entity/woodOcta.png");
-	private static final ResourceLocation aluminiumOctaTextureLocation = new ResourceLocation(DroneMod.modID + ":textures/entity/aluminiumOcta.png");
+@SideOnly(Side.CLIENT)
+public class RenderOctacopter extends RenderLiving{
+	//private static final ResourceLocation caneOctaTextureLocation = new ResourceLocation(DroneMod.modID + ":textures/entity/caneOcta.png");
+	//private static final ResourceLocation woodOctaTextureLocation = new ResourceLocation(DroneMod.modID + ":textures/entity/woodOcta.png");
+	//private static final ResourceLocation aluminiumOctaTextureLocation = new ResourceLocation(DroneMod.modID, "textures/entity/test.png");
 	
-	private static final ResourceLocation octaModelLocation = new ResourceLocation(DroneMod.modID + ":models/entity/octa.obj");
-	private static final IModelCustom octaModel = AdvancedModelLoader.loadModel(octaModelLocation);
-	
-	public RenderOctacopter() {
-		super(octaModel);
-	}
-
-	@Override
-	public void doRender(Entity var1, double x, double y, double z, float var8, float var9) {
-		// Render chests, modules, batteries in appropriate locations for octacopters.
-		super.doRender(var1, x, y, z, var8, var9);
+	public RenderOctacopter(ModelOctaCopter par1ModelBase, float par2) {
+		super(par1ModelBase, par2);
 	}
 
 	@Override
@@ -52,9 +36,10 @@ public class RenderOctacopter extends RenderDrone {
 		}
 		
 		if(d instanceof EntityAluminiumMediumOctacopter || d instanceof EntityAluminiumStrongOctacopter) {
-			return aluminiumOctaTextureLocation;
+			return new ResourceLocation(DroneMod.modID, "textures/entity/test.png");
 		}else if(d instanceof EntityWoodMediumOctacopter || d instanceof EntityWoodStrongOctacopter ){
-			return woodOctaTextureLocation;
+			//Put back to wood once finished
+			return new ResourceLocation(DroneMod.modID.toLowerCase(), "textures/entity/test.png");
 		}else{
 			Logger.logOut("Wrong type of drone, not an Octa! No texture loaded.");
 			return null;
